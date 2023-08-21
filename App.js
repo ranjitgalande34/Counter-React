5ea11d487aa3edec3c06aeb6f043b8e9e3e7be7d
Ranjit Galande <105547231+ranjitgalande34@users.noreply.github.com>
2023-08-21 20:19:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import Counter from "./Components/Counter.js"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ Counter.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './Counter.css';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <div className="buttons">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
